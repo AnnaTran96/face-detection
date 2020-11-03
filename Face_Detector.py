6b840ad62d8a9fc72503614339a3c0b2517f6d3c
Anna Tran <annatran@annas-air.mynet>
2020-11-03 17:54:17 +0000

    break loop when keys are pressed

--- Face_Detector.py
+++ Face_Detector.py
@@ -3,9 +3,29 @@ from random import randrange
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('img2.jpg')
+# img = cv2.imread('img2.jpg')
+webcam = cv2.VideoCapture(0)
 
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Iterate over frames
+
+while True:
+
+    successful_frame_read, frame = webcam.read()
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+    for (x, y, w, h) in face_coordinates:
+        # cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 6)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255,0), 6)
+
+    
+    cv2.imshow('Webcam Face Detector', frame)
+    key = cv2.waitKey(1)
+
+    if key==81 or key==113 or key==27:
+        break
+
+"""
 
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 print(face_coordinates)
@@ -20,5 +40,7 @@ for (x, y, w, h) in face_coordinates:
 # This shows the name of the window that pops up: Clever programmer face detector followed by the image you want to show which we have defined above.
 cv2.imshow('Face Detector', img)
 cv2.waitKey()
+
+"""
 
 print("Hello")
