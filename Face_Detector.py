6b1a7c2abf10d438c8e2f3c64b45699cdc78486f
Anna Tran <annatran@annas-air.mynet>
2020-11-03 16:16:46 +0000

    add a for loop

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,4 +1,5 @@
 import cv2
+from random import randrange
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -10,8 +11,11 @@ face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 print(face_coordinates)
 
 # Draw rectangles
-(x, y, w, h) = face_coordinates[0]
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# (x, y, w, h) = face_coordinates[0]
+
+# Detect multiple people - we need to loop through the array
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 6)
 
 # This shows the name of the window that pops up: Clever programmer face detector followed by the image you want to show which we have defined above.
 cv2.imshow('Face Detector', img)
